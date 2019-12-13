dc6c0e362e7c1ab3558cd4038fe9d93136b19e26
NWaka-1415 <yuuki.ohta.chintap@gmail.com>
2019-12-13 12:34:00 +0900

    ネーム変更およびCEBlockの新規作成，IncludeBlock用のフィールド作成

---
+++ src/objects/IncludeField.java
@@ -0,0 +1,142 @@
+package objects;
+
+import objects.block.Block;
+
+public class IncludeField extends AppletObject {
+    private int x, y;
+    private int w, h, dW, dH;
+
+    private Block includeBlock;
+    private Block parentBlock;
+
+    //ブロックが入れます
+    private boolean includeFlag;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getW() {
+        return w;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public int getDW() {
+        return dW;
+    }
+
+    public int getDH() {
+        return dH;
+    }
+
+    public boolean includeFlag() {
+        return includeFlag;
+    }
+
+    public IncludeField(Block parentBlock) {
+        this.parentBlock = parentBlock;
+        includeBlock = null;
+    }
+
+    public IncludeField(Block parentBlock, Block block) {
+        this.parentBlock = parentBlock;
+        includeBlock = block;
+    }
+
+    public IncludeField setPos(int x, int y) {
+        this.x = x;
+        this.y = y;
+        return this;
+    }
+
+    public IncludeField setDefaultSize(int w, int h) {
+        this.dW = w;
+        this.dH = h;
+        return this;
+    }
+
+    public IncludeField setSize(int w, int h) {
+        this.w = w;
+        this.h = h;
+        return this;
+    }
+
+    public IncludeField resetSize() {
+        w = dW;
+        h = dH;
+        return this;
+    }
+
+    public Block includeBlock() {
+        return includeBlock;
+    }
+
+    public void enterBlock(Block block) {
+        includeBlock = block;
+
+        this.w = dW + block.w;
+
+        enterDisplay(block);
+    }
+
+    public void enterDisplay(Block block) {
+        int addX = this.x - block.x;
+        int addY = this.y - block.y;
+        block.move(addX, addY);
+    }
+
+    @Override
+    public void display() {
+        if (includeBlock == null) {
+            applet.strokeWeight(3);
+            applet.stroke(90, 0, 180);
+            applet.fill(255);
+            applet.rect(x, y, dW, dH, 5);
+        } else {
+            applet.strokeWeight(3);
+            applet.stroke(90, 0, 180);
+            applet.fill(255);
+            applet.rect(x, y, w, h, 5);
+        }
+    }
+
+    public String code() {
+        if (includeBlock == null) return "";
+        return includeBlock.code();
+    }
+
+    @Override
+    public void move(int addX, int addY) {
+        x += addX;
+        y += addY;
+    }
+
+    public void outBlock() {
+        includeBlock.parentBlock = null;
+        includeBlock = null;
+        resetSize();
+    }
+
+    public boolean canIncludeBlock(Block block) {
+        if (!block.connectableElement()) return false;
+        int bx = block.x;
+        int by = block.y;
+        boolean result = x <= bx && bx <= x + dW &&
+                y <= by + block.h / 2 && by + block.h / 2 <= y + dH;
+        includeFlag = result;
+        return result;
+    }
+
+    @Override
+    public boolean isPressed() {
+        return x <= applet.mouseX && applet.mouseX <= x + w &&
+                y <= applet.mouseY && applet.mouseY <= y + h;
+    }
+}
---
+++ src/objects/block/element/ConditionalExpressionBlock.java
@@ -0,0 +1,156 @@
+package objects.block.element;
+
+import objects.ComboBox;
+import objects.IncludeField;
+import objects.block.Block;
+import objects.block.IHaveContent;
+
+public class ConditionalExpressionBlock extends Block implements IHaveContent {
+
+    private final String[] contents = new String[]{" == ", " != ", " < ", " <= ", " > ", " >= "};
+    private ComboBox comboBox;
+
+    public final IncludeField[] includeFields = new IncludeField[2];
+
+    public ConditionalExpressionBlock(int x, int y, int w, int h) {
+        super("", x, y, w, h);
+        initialize();
+    }
+
+    public ConditionalExpressionBlock(int x, int y, int w) {
+        super("", x, y, w);
+        initialize();
+    }
+
+    public ConditionalExpressionBlock(int x, int y) {
+        super("", x, y);
+        initialize();
+    }
+
+    private void initialize() {
+        comboBox = new ComboBox(contents, x, y + h / 10);
+
+        includeFields[0] = new IncludeField(this);
+        includeFields[1] = new IncludeField(this);
+        int count = 0;
+        int tmpW = w, tmpH = h;
+        w += comboBox.getW();
+        h += 10;
+        for (IncludeField includeField : includeFields) {
+            includeField.setSize(tmpW / 2, tmpH).setDefaultSize(tmpW / 4, tmpH)
+                    .setPos(count == 0 ? x + 5 : x + w - 5 - includeField.getW() / 2, y + h / 10);
+            count++;
+        }
+        comboBox.setPos(x + getDw() / 2, comboBox.getY() + 5);
+
+        setDefaultSize(w, h);
+    }
+
+    @Override
+    public void move(int addX, int addY) {
+        super.move(addX, addY);
+        comboBox.move(addX, addY);
+        for (IncludeField includeField : includeFields) {
+            includeField.move(addX, addY);
+        }
+    }
+
+    @Override
+    public boolean isPressedContent() {
+        return comboBox.isPressed();
+    }
+
+    @Override
+    public void setFocusToContent() {
+        comboBox.focus();
+        applet.setTopBlock(this);
+        ComboBox.openOrClose();
+    }
+
+    @Override
+    public boolean haveContent() {
+        return comboBox != null;
+    }
+
+    @Override
+    public void display() {
+        applet.strokeWeight(3);
+        applet.stroke(90, 0, 180);
+        applet.fill(128, 0, 225);
+        applet.rect(x, y, w, h, 5);    //最後の引数は矩形の角の曲がり具合
+        for (IncludeField includeField : includeFields) {
+            includeField.display();
+        }
+        comboBox.display();
+    }
+
+    @Override
+    public void exchangeCode() {
+        setCode(includeFields[0].code() + comboBox.getSelectContent() + includeFields[1].code());
+    }
+
+    @Override
+    public boolean canConnect(Block block) {
+        return false;
+    }
+
+    @Override
+    public boolean connectable() {
+        return false;
+    }
+
+    @Override
+    public void enterBlock(Block block) {
+        for (IncludeField includeField : includeFields) {
+            if (includeField.includeBlock() != null && includeField.includeFlag()) {
+                includeField.enterBlock(block);
+                block.parentBlock = this;
+                this.w = getDw() + block.w;
+            }
+        }
+    }
+
+    @Override
+    public void enter() {
+        for (IncludeField includeField : includeFields) {
+            if (includeField.includeBlock() != null) includeField.enterDisplay(includeField.includeBlock());
+        }
+    }
+
+    @Override
+    protected void enterDisplay(Block block) {
+        // pass
+    }
+
+    @Override
+    public void outBlock() {
+        for (IncludeField includeField : includeFields) {
+            if (!includeField.includeFlag()) includeField.outBlock();
+        }
+        w = getDw();
+        h = getDh();
+    }
+
+    @Override
+    public boolean canConnectElement(Block block) {
+        if (!block.connectableElement()) return false;
+        int bx = block.x;
+        int by = block.y;
+        boolean result;
+        for (IncludeField includeField : includeFields) {
+            result = includeField.canIncludeBlock(block);
+            if (result) return result;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean connectableElement() {
+        return true;
+    }
+
+    @Override
+    public boolean isPressed() {
+        return super.isPressed() || comboBox.isPressed();
+    }
+}
